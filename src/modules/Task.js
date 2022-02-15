f34d70dc9548da41ebd136ff12f43c0e41c398fd
amira <amiraabouhadid@gmail.com>
2022-02-15 20:19:25 +0200

    create clone task list structure

---
+++ src/modules/Task.js
@@ -0,0 +1,7 @@
+export default class Task {
+  constructor(description, complete, index) {
+    this.description = description;
+    this.complete = complete;
+    this.index = index;
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,10 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
     ],
   },
   plugins: [
