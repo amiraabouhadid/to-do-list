abb97e706933e9fdbba755c8296ec956a4b52fcb
amira <amiraabouhadid@gmail.com>
2022-02-15 14:44:20 +0200

    add webpack template

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import _ from "lodash";
 import "./style.css";
-function component() {
-  const element = document.createElement("div");
-
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/js/fontawesome";
+let tasks = [];
+function addTask() {}
+function displayTask() {
+  const element = document.createElement("li");
+  element.innerHTML = "hey";
+  element.style.color = "black";
   return element;
 }
 
-document.body.appendChild(component());
+document.body.appendChild(displayTask());
